2cf2fe75b84db6eca8bb751fe22f27741372cea6
Jocabed Ríos <j0c4b3d@gmail.com>
2022-11-04 11:09:04 -0600

    First phase completed.

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 	var root xpath.NodeNavigator
 	root = xmlquery.CreateXPathNavigator(doc)
 
-	fmt.Println("Lines:")
+	fmt.Println("SUBWAY LINES:")
 	expr := xpath.MustCompile("//Folder[name='Líneas de Metro']/Placemark/name")
 	lines := expr.Evaluate(root)
 	iter := lines.(*xpath.NodeIterator)
@@ -44,27 +44,28 @@ func main() {
 		fmt.Println()
 		line := iter.Current().Value()
 		fmt.Println(line)
-		fmt.Println("\tStations:")
+		fmt.Println("\tSTATIONS:")
 		expr2 := xpath.MustCompile("//Folder[name='Estaciones de Metro']/Placemark[contains(description,'" + line + "')]/name")
 		stations := expr2.Evaluate(root)
 		iter2 := stations.(*xpath.NodeIterator)
 
-		for iter2.MoveNext() {
+		for i := 1; iter2.MoveNext(); i++ {
 			station := iter2.Current().Value()
-			fmt.Println("\t" + station)
-			/* expr = xpath.MustCompile("//Folder[name='Estaciones de Metro']/Placemark[name=" + station + "]/Point/coordinates")
-			val := expr.Evaluate(root)
-			iter := val.(*xpath.NodeIterator)
-			for iter.MoveNext() {
-				fmt.Print(iter.Current().Value())
-			} */
+			fmt.Printf("\t%d. %s", i, strings.Trim(station, "\n"))
 
-			/* expr, err := xpath.Compile("//Folder[name='Estaciones de Metro']/Placemark[name=" + station + "]/Point/coordinates")
+			expr3, err := xpath.Compile("//Folder[name='Estaciones de Metro']/Placemark[name='" + station + "']/Point/coordinates")
 			if err != nil {
 				panic(err)
 			}
-			val := expr.Evaluate(root)
-			fmt.Print(val) */
+
+			var root2 xpath.NodeNavigator
+			root2 = xmlquery.CreateXPathNavigator(doc)
+			coor := expr3.Evaluate(root2)
+
+			iter3 := coor.(*xpath.NodeIterator)
+			for iter3.MoveNext() {
+				fmt.Printf(", %s", strings.Trim(strings.Trim(iter3.Current().Value(), "\n"), " "))
+			}
 		}
 
 	}
