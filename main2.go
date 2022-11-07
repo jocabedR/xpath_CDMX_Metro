bcb786a14606b57307108283726ec98ffa5f38f5
Jocabed Ríos <j0c4b3d@gmail.com>
2022-11-07 16:21:56 -0600

    The program returns the path using the id's of nodes. It's missing to return node's name and the line of every segment.

--- main2.go
+++ main2.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/antchfx/xmlquery"
 	"github.com/antchfx/xpath"
+	"github.com/yourbasic/graph"
 )
 
 type node struct {
@@ -19,11 +20,11 @@ type node struct {
 }
 
 type segment struct {
-	id       int
-	origin   string
-	destiny  string
-	line     string
-	distance float64
+	name        string
+	line        string
+	origin      int
+	destination int
+	distance    int64
 }
 
 func main() {
@@ -40,7 +41,6 @@ func main() {
 	}
 
 	xmlContent := string(content)
-
 	origin := os.Args[2]
 	destination := os.Args[3]
 
@@ -48,16 +48,18 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	countexpr, err := xpath.Compile("count(//Folder[name='Estaciones de Metro']/Placemark)")
+	numberStations := int(countexpr.Evaluate(xmlquery.CreateXPathNavigator(doc)).(float64))
 
 	var root xpath.NodeNavigator
 	root = xmlquery.CreateXPathNavigator(doc)
 
 	nodes := getNodes(root)
-	fmt.Println(nodes[origin].id)
-	fmt.Println(nodes[destination].id)
-	//segments := getSegments(root)
-	//fmt.Println(segments)
+	segments := getSegments(root, nodes)
 
+	fillGraph(numberStations, segments, nodes[origin].id, nodes[destination].id)
+
+	//calculatePath(g, nodes[origin].id, nodes[destination].id)
 }
 
 func getNodes(root xpath.NodeNavigator) map[string]node {
@@ -82,27 +84,24 @@ func getNodes(root xpath.NodeNavigator) map[string]node {
 			}
 		}
 	}
-
 	return nodes
 }
 
-/* func getSegments(root xpath.NodeNavigator) map[string]segment {
-	var segments = make(map[string]segment)
+func getSegments(root xpath.NodeNavigator, nodes map[string]node) map[int]segment {
+	var segments = make(map[int]segment)
 
+	// GET LINES
 	expr := xpath.MustCompile("//Folder[name='Líneas de Metro']/Placemark/name")
 	lines := expr.Evaluate(root)
-	iter := lines.(*xpath.NodeIterator)
+	iterLines := lines.(*xpath.NodeIterator)
 
-	for iter.MoveNext() {
+	counter := 0
 
-		fmt.Println()
-		line := iter.Current().Value()
-		fmt.Println(line)
+	for iterLines.MoveNext() {
 
-		expr3, err := xpath.Compile("//Folder[name='Líneas de Metro']/Placemark[name='" + line + "']/LineString/coordinates")
-		if err != nil {
-			panic(err)
-		}
+		line := iterLines.Current().Value()
+		// GET STATIONS COORDINATES
+		expr3 := xpath.MustCompile("//Folder[name='Líneas de Metro']/Placemark[name='" + line + "']/LineString/coordinates")
 
 		coor := expr3.Evaluate(root)
 		var parseCor []string
@@ -128,35 +127,33 @@ func getNodes(root xpath.NodeNavigator) map[string]node {
 
 			for iter2.MoveNext() {
 				station := iter2.Current().Value()
-				fmt.Printf("\t%d. %s\n", j, station)
-				fmt.Printf("\t|%s|\n", cordinate)
 
-				var distanceNext float64
+				var distanceNext int64
 				if j < len(parseCor)-1 {
+					var destination string
 					distanceNext = distance(cordinate, nextcordinate)
-					fmt.Printf("\t|%f|\n\n", distanceNext)
 
-					segments[name] = node{
-						/*id       int
-						origin   string
-						destiny  string
-						line     string
-						distance float64
-						,
+					expr3 := xpath.MustCompile("//Folder[name='Estaciones de Metro']/Placemark/Point[contains(coordinates,'" + nextcordinate + "')]/preceding-sibling::*[3]")
+					dest := expr3.Evaluate(root)
+					iter3 := dest.(*xpath.NodeIterator)
+
+					for iter3.MoveNext() {
+						destination = iter3.Current().Value()
 					}
 
+					name := station + "-" + destination
+					segments[counter] = segment{
+						name, line, nodes[station].id, nodes[destination].id, distanceNext,
+					}
+					counter++
 				}
-
 			}
-
 		}
-
 	}
-
 	return segments
-} */
+}
 
-func distance(coord0, coord1 string) float64 {
+func distance(coord0, coord1 string) int64 {
 	coordenates0 := strings.Split(coord0, ",")
 	coordenates1 := strings.Split(coord1, ",")
 
@@ -165,5 +162,17 @@ func distance(coord0, coord1 string) float64 {
 	y0, _ := strconv.ParseFloat(coordenates0[1], 64)
 	y1, _ := strconv.ParseFloat(coordenates1[1], 64)
 
-	return math.Sqrt(((x0 - x1) * (x0 - x1)) + ((y0 - y1) * (y0 - y1)))
+	return int64(math.Sqrt(((x0-x1)*(x0-x1))+((y0-y1)*(y0-y1))) * 1000000)
+}
+
+func fillGraph(numberStations int, segments map[int]segment, o int, d int) {
+	g := graph.New(numberStations)
+
+	for i := 0; i < len(segments); i++ {
+		g.AddBothCost(segments[i].origin, segments[i].destination, segments[i].distance)
+	}
+
+	path, dist := graph.ShortestPath(g, o, d)
+	fmt.Println("Path: ", path)
+	fmt.Printf("Length: %d m", dist)
 }
